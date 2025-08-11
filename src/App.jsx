a429cd969c6ced192e13a42f37cad3b77a996e12
Shubig <shubhamnayak.sn1@gmail.com>
2025-08-11 22:18:16 +0530

    Fix file name casing for Linux (Vercel) compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import Header from './resources/header'
-import Footer from './resources/footer'
+import Header from './resources/Header1'
+import Footer from './resources/Footer1'
 import Home from './resources/Home'
 import { Routes, Route } from 'react-router-dom';
 import About from './resources/About';
 import Services from './resources/Services';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
