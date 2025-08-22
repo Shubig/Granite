18aa2cdc758a3a69654c6e471490ebef48df4e5d
Shubig <shubhamnayak.sn1@gmail.com>
2025-08-22 21:34:41 +0530

    Updated File

--- src/resources/Contact1.jsx
+++ src/resources/Contact1.jsx
@@ -11,9 +11,11 @@ function Contact() {
             <div className="flex flex-wrap">
             <p className="text-2xl font-bold font-sans"> E-Mail - shreebalagigranites22</p>
             <p className="text-2xl font-bold font-sans"> @gmail.com</p>
-            </div><br /><br />
+            </div><br />
+            <div className=''><button className='bg-amber-300 rounded-2xl font-bold px-5 py-3 hover:bg-green-800 hover:text-amber-50 '><a href="https://wa.link/3uyac9">Whattsapp</a></button></div>
             <p className="text-2xl font-bold font-sans">We look forward to hearing from you! Whether it’s a quick call or a detailed email, we’re always ready to assist and make your experience smooth and enjoyable. Your ideas and requirements matter to us, and we can’t wait to connect.</p>
         </div>
+        
         </div></>   )
 }
 
