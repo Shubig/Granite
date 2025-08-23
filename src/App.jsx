a547693accd8fbfc4c10c1c7dba6748aa5d6d501
Shubig <shubhamnayak.sn1@gmail.com>
2025-08-23 13:32:44 +0530

    Updated File

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,42 @@ import { Routes, Route } from 'react-router-dom';
 import About from './resources/About';
 import Services from './resources/Services';
 import Contact from './resources/Contact1';
+import {FloatingWhatsApp} from "react-floating-whatsapp";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+<FloatingWhatsApp
+phoneNumber='6371903644'
+avatar='./Founder.jpg'
+accountName='Shree Balaji Granites'
+statusMessage='Typically replies within 30 minutes'
+placeholder='Type your message here.....'
+messageDelay={3}
+allowClickAway={true}
+allowEsc={true}
+chatboxHeight={400}
+notification={true}
+notificationDelay={10}
+notificationSound={true}
+notificationLoop={3}
+style={{
+  backgroundColor:"#25D366",
+  color:"black",
+}}
+chatboxStyle={{
+  backgroundColor:"#F0F0F0",
+}}
+
+/>
+
+
+
+
+
+
 <Header />           
   <Routes>
     <Route path="/" element={<Home />} />
